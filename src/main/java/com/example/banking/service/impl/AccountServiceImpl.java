470db1ff7330d9de6b98f8c3a480c0de7017db82
nusrat <nusratcode14@gmail.com>
2024-03-24 15:23:08 +0600

    add transaction history management

--- src/main/java/com/example/banking/service/impl/AccountServiceImpl.java
+++ src/main/java/com/example/banking/service/impl/AccountServiceImpl.java
@@ -3,12 +3,15 @@ package com.example.banking.service.impl;
 import com.example.banking.dto.AccountDto;
 import com.example.banking.dto.TransferFundDto;
 import com.example.banking.entity.Account;
+import com.example.banking.entity.Transaction;
 import com.example.banking.exception.AccountException;
 import com.example.banking.mapper.AccountMapper;
 import com.example.banking.repository.AccountRepository;
+import com.example.banking.repository.TransactionRepository;
 import com.example.banking.service.AccountService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,8 +19,14 @@ import java.util.stream.Collectors;
 public class AccountServiceImpl implements AccountService {
 
     private AccountRepository accountRepository;
-    public AccountServiceImpl(AccountRepository accountRepository){
+    private TransactionRepository transactionRepository;
+
+    private static final String TRANSACTION_TYPE_DEPOSIT = "DEPOSIT";
+    private static final String TRANSACTION_TYPE_WITHDRAW = "WITHDRAW";
+    private static final String TRANSACTION_TYPE_TRANSFER = "TRANSFER";
+    public AccountServiceImpl(AccountRepository accountRepository, TransactionRepository transactionRepository){
         this.accountRepository = accountRepository;
+        this.transactionRepository = transactionRepository;
     }
     @Override
     public AccountDto createAccount(AccountDto accountDto) {
@@ -43,6 +52,13 @@ public class AccountServiceImpl implements AccountService {
         double total = account.getBalance() + amount;
         account.setBalance(total);
         Account savedAccount = accountRepository.save(account);
+
+        Transaction transaction = new Transaction();
+        transaction.setAccountId(id);
+        transaction.setAmount(amount);
+        transaction.setTransactionType(TRANSACTION_TYPE_DEPOSIT);
+        transaction.setTimestamp(LocalDateTime.now());
+        transactionRepository.save(transaction);
         return AccountMapper.mapToAccountDto(savedAccount);
     }
 
@@ -53,9 +69,19 @@ public class AccountServiceImpl implements AccountService {
                 .findById(id)
                 .orElseThrow(()-> new AccountException("Account does not exists"));
 
+        if(account.getBalance() < amount){
+            throw new RuntimeException("Insufficient amount");
+        }
         double total = account.getBalance() - amount;
         account.setBalance(total);
         Account withdrawAccount = accountRepository.save(account);
+
+        Transaction transaction = new Transaction();
+        transaction.setAccountId(id);
+        transaction.setAmount(amount);
+        transaction.setTransactionType(TRANSACTION_TYPE_WITHDRAW);
+        transaction.setTimestamp(LocalDateTime.now());
+        transactionRepository.save(transaction);
         return AccountMapper.mapToAccountDto(withdrawAccount);
     }
 
@@ -87,11 +113,22 @@ public class AccountServiceImpl implements AccountService {
     Account accountTo = accountRepository.findById(transferFundDto.toAccountId())
             .orElseThrow(()-> new AccountException("Account does not exists"));
 
+    if(fromAccount.getBalance() < transferFundDto.amount()){
+        throw new RuntimeException("Insufficient fund");
+    }
     // Debit the amount from fromAccount object
     fromAccount.setBalance(fromAccount.getBalance() - transferFundDto.amount());
 
     // Credit the amount to toAccount object
     accountTo.setBalance(accountTo.getBalance() + transferFundDto.amount());
+
+    Transaction transaction = new Transaction();
+    transaction.setAccountId(transferFundDto.fromAccountId());
+    transaction.setAmount(transferFundDto.amount());
+    transaction.setTransactionType(TRANSACTION_TYPE_TRANSFER);
+    transaction.setTimestamp(LocalDateTime.now());
+    transactionRepository.save(transaction);
+
 
     accountRepository.save(fromAccount);
     accountRepository.save(accountTo);
